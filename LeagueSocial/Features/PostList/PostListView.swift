f36fcca965f510ad1aae33a2efec98573f39808c
Arpit Williams <arpitwilliams88@gmail.com>
2025-04-03 18:10:38 +0530

    Added logic & UI for PostList feature

--- LeagueSocial/Features/PostList/PostListView.swift
+++ LeagueSocial/Features/PostList/PostListView.swift
@@ -8,11 +8,79 @@
 import SwiftUI
 
 struct PostListView: View {
+    @State private var selectedUser: User?
+    @State private var showExitAlert = false
+    @Environment(\.dismiss) private var dismiss
+    @StateObject private var viewModel: PostListViewModel
+    
+    init(viewModel: PostListViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // List View
+        List {
+            ForEach(viewModel.posts) { post in
+                PostRowView(post: post) {
+                    selectedUser = post.user
+                }
+                .listRowInsets(.init())
+                .listRowBackground(Color.clear)
+            }
+        }
+        // List Background
+        .scrollContentBackground(.hidden)
+        .background(Color(.mint.opacity(0.2)))
+        // Navigation bar
+        .navigationTitle("Posts")
+        .navigationBarBackButtonHidden(true)
+        // Toolbar
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                // Logout button
+                Button(viewModel.isGuest ? "Exit" : "Logout") {
+                    if viewModel.isGuest {
+                        showExitAlert = true
+                    } else {
+                        viewModel.logout()
+                        dismiss()
+                    }
+                }
+            }
+        }
+        // Progress view
+        .overlay {
+            if viewModel.isLoading {
+                ProgressView()
+            }
+        }
+        // Error Alert
+        .alert("Error", isPresented: .constant(viewModel.errorMessage != nil)) {
+            Button("OK", role: .cancel) {
+                viewModel.errorMessage = nil
+            }
+        } message: {
+            Text(viewModel.errorMessage ?? "")
+        }
+        // Exit Alert
+        .alert("Exit Guest Session?", isPresented: $showExitAlert) {
+            Button("Cancel", role: .cancel) { }
+            Button("Exit", role: .destructive) { dismiss() }
+        } message: {
+            Text("You’re currently logged in as a guest.")
+        }
+        // User Info View
+        .sheet(item: $selectedUser) { _ in }
+        // Load Posts
+        .task {
+            await viewModel.loadPosts()
+        }
     }
 }
 
 #Preview {
-    PostListView()
+    NavigationStack {
+        PostListView(viewModel: PostListViewModel(api: MockAPIService(), isGuest: true))
+            .environment(\.dependencies, Container(apiService: MockAPIService()))
+    }
 }
--- LeagueSocial/Features/PostList/PostRowView.swift
+++ LeagueSocial/Features/PostList/PostRowView.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct PostRowView: View {
+    let post: Post
+    let onTapGesture: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .top, spacing: 12) {
+            VStack(spacing: 8) {
+                
+                // Avator image
+                AsyncImage(url: post.user?.avatar) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                            .frame(width: 60, height: 60)
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 60, height: 60)
+                            .clipShape(Circle())
+                    case .failure:
+                        Image(systemName: "person.crop.circle.fill")
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                            .foregroundStyle(Color.gray)
+                    default:
+                        EmptyView()
+                    }
+                }
+                
+                // Username
+                Text(post.user?.username ?? "unknown")
+                    .bold()
+                    .font(.subheadline)
+                    .foregroundColor(.accentColor)
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: 72, alignment: .center)
+            }
+            .onTapGesture { onTapGesture() }
+            
+            // Post title & description
+            VStack(alignment: .leading, spacing: 8) {
+                Text(post.title)
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text(post.body)
+                    .font(.callout)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            
+        }
+        .padding(.vertical)
+        .padding(.horizontal, 8)
     }
 }
 
 #Preview {
-    PostRowView()
+    PostRowView(post: Post.mock.first!, onTapGesture: { })
+        .background(Color.mint.opacity(0.2))
 }
